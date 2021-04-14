ef2c9c15370621d55f3ef8c443cd73861befa655
Georgy Levochkin <aspect71rus@gmail.com>
2021-04-14 12:52:47 +0300

    v1

--- src/test/java/SelenideTest.java
+++ src/test/java/SelenideTest.java
@@ -1,14 +1,31 @@
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selectors;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static java.time.Duration.ofMillis;
 
 public class SelenideTest {
+    int random = (int) ( Math.random()* (30 - 3 + 1) + 3 );
+    String date = LocalDate.now().plusDays(random).format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
 
     @Test
     void shouldValidResponse() {
         open("http://localhost:9999/");
         $("[placeholder='Город']").setValue("Москва");
-        sleep(5000);
+        $("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+        $("[placeholder='Дата встречи']").setValue(date);
+        $("[name='name']").setValue("Тестов Тестович");
+        $("[name='phone']").setValue("+79109999999");
+        $(".checkbox").click();
+        $(".button").click();
+        $(byText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
     }
 }
