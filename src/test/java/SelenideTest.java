f24431be30577e1229f6e0ee4a02c534272eec91
Georgy Levochkin <aspect71rus@gmail.com>
2021-04-14 14:43:55 +0300

    v2

--- src/test/java/SelenideTest.java
+++ src/test/java/SelenideTest.java
@@ -1,5 +1,6 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selectors;
+import com.codeborne.selenide.conditions.CssValue;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
@@ -9,6 +10,7 @@ import java.time.format.DateTimeFormatter;
 
 
 import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 import static java.time.Duration.ofMillis;
 
@@ -17,7 +19,7 @@ public class SelenideTest {
     String date = LocalDate.now().plusDays(random).format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
 
     @Test
-    void shouldValidResponse() {
+    void shouldValidForm() {
         open("http://localhost:9999/");
         $("[placeholder='Город']").setValue("Москва");
         $("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
@@ -27,5 +29,76 @@ public class SelenideTest {
         $(".checkbox").click();
         $(".button").click();
         $(byText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    void shouldEmptyForm() {
+        open("http://localhost:9999/");
+        $(".button").click();
+        $(byText("Поле обязательно для заполнения")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    void shouldNotValidCity() {
+        open("http://localhost:9999/");
+        $("[placeholder='Город']").setValue("Ясногорск");
+        $("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+        $("[placeholder='Дата встречи']").setValue(date);
+        $("[name='name']").setValue("Тестов Тестович");
+        $("[name='phone']").setValue("+79109999999");
+        $(".checkbox").click();
+        $(".button").click();
+        $(byText("Доставка в выбранный город недоступна")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    void shouldEmptyCity() {
+        open("http://localhost:9999/");
+        $("[placeholder='Город']").setValue("Москва");
+        $("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+        $("[placeholder='Дата встречи']").setValue(date);
+        $("[name='name']").setValue("Тестов Тестович");
+        $("[name='phone']").setValue("+79109999999");
+        $(".checkbox").click();
+        $(".button").click();
+        $(byText("Поле обязательно для заполнения")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    void shouldNotValidDate() {
+        open("http://localhost:9999/");
+        $("[placeholder='Город']").setValue("Москва");
+        $("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+        $("[placeholder='Дата встречи']").setValue(LocalDate.now().plusDays(-1).format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+        $("[name='name']").setValue("Тестов Тестович");
+        $("[name='phone']").setValue("+79109999999");
+        $(".checkbox").click();
+        $(".button").click();
+        $(byText("Заказ на выбранную дату невозможен")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    void shouldNotValidTel() {
+        open("http://localhost:9999/");
+        $("[placeholder='Город']").setValue("Москва");
+        $("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+        $("[placeholder='Дата встречи']").setValue(date);
+        $("[name='name']").setValue("Тестов Тестович");
+        $("[name='phone']").setValue("+7910999999");
+        $(".checkbox").click();
+        $(".button").click();
+        $(withText("Телефон указан неверно. Должно быть 11 цифр")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    void shouldNotClickCheckbox() {
+        open("http://localhost:9999/");
+        $("[placeholder='Город']").setValue("Москва");
+        $("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+        $("[placeholder='Дата встречи']").setValue(date);
+        $("[name='name']").setValue("Тестов Тестович");
+        $("[name='phone']").setValue("+79109999999");
+        $(".button").click();
+        $(withText("Я соглашаюсь")).shouldBe(Condition.cssValue("color", "rgba(255, 92, 92, 1)"));
     }
 }
